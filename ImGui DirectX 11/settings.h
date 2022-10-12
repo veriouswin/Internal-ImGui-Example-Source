c991018d04b2557ceef698aa7976ab9ff3dcab76
veriouswin <113793162+veriouswin@users.noreply.github.com>
2022-10-12 19:31:16 +0000

    Add files via upload

---
+++ ImGui DirectX 11/settings.h
@@ -0,0 +1,14 @@
+namespace Settings
+{
+	static bool aimbot			= false;
+	static bool aimfov			= false;
+	static bool spinbot			= false;
+
+	static int tabs				= 0;
+
+	static float aimfovvalue	= 50.f;
+
+	namespace Colors {
+		
+	}
+}
